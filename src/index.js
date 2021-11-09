9cc705185a4ea0769552b41c09e8457ca2c06132
Gordon <gordon.tinyefuza@gmail.com>
2021-11-09 20:16:10 +0300

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const scoreData = [
+  { name: 'name', score: 100 },
+  { name: 'name', score: 53 },
+  { name: 'name', score: 67 },
+  { name: 'name', score: 51 },
+  { name: 'name', score: 23 },
+  { name: 'name', score: 98 },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  return element;
+function loadTable() {
+  const table = document.querySelector('.scoreboard-table');
+
+  scoreData.forEach((data) => {
+    const tr = document.createElement('tr');
+    tr.className = 'table-row';
+    const td = document.createElement('td');
+    td.className = 'table-data';
+    td.innerHTML = `${data.name}: ${data.score}`;
+
+    tr.appendChild(td);
+    table.appendChild(tr);
+  });
 }
 
-document.body.appendChild(component());
+loadTable();
