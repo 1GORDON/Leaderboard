5d5c091e906242298b2407e5269a954dcd09499b
Gordon <gordon.tinyefuza@gmail.com>
2021-11-10 22:23:41 +0300

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import loadTable from './loadTable.js';
+import addScore from './addScore.js';
+import recentScores from './recentScores';
+import refresh from './refresh';
+import { getScores, gameID } from './api.js';
 import './style.css';
 
+const loadTable = () => {
+  window.onload() = () => {
+    if (gameID()) {
+      getScores(gameID)
+      .then((scores) => recentScores(scores)); 
+     }
+  };
+};
+
+addScore();
+refresh();
 loadTable();
