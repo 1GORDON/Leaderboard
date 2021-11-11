dd29cc2657b9a5fe5ffd3a36dbd4a0e8e133568f
Gordon <gordon.tinyefuza@gmail.com>
2021-11-11 12:53:16 +0300

    Remove comments

--- src/api.js
+++ src/api.js
@@ -1,33 +1,3 @@
-// const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-
-// const createGame = async () => {
-//   try {
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         name: 'newGame',
-//       }),
-//       header: {
-//         'content-type': 'application/json; charset=UTF-8',
-//       },
-//     });
-//     const newPost = await response.json();
-//     const gameId = newPost.result.split(' ')[3];
-//     localStorage.setItem('gameID', JSON.stringify(gameId));
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// };
-
-// const gameID = () => {
-//   const id = JSON.parse(localStorage.getItem('gameID'));
-
-//   if (!id) {
-//     createGame();
-//   }
-//   return id;
-// };
-
 const saveToApi = async (username, userscore) => {
   try {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/v4n9rcNTL0SXbCD8IPLG/scores/', {
